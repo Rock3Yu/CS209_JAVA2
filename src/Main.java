885484bfbb2533082125bed9ca604472ee6fb9c0
rockyu <rockyu787@gmail.com>
2023-02-22 14:11:33 +0800

    Week2_Lecture: Test codes

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 @SuppressWarnings("checkstyle:MissingJavadocType")
 public class Main {
 
+    @SuppressWarnings("checkstyle:MissingJavadocMethod")
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        List<String> strList = new ArrayList<>();
+        strList.add(null);
+        strList.add("ss");
+        strList.add("");
+        strList.add(null);
+        System.out.println(strList);
+
+        Set<Integer> intSet = new HashSet<>();
+        intSet.add(1);
+        intSet.add(null);
+        System.out.println(intSet);
+
+        Map<Integer, String> map = new HashMap<>();
+        map.put(12013027, "YU Kunyi");
+        map.put(33, "Rock");
+        System.out.println(map.keySet());
+        System.out.println(map.entrySet());
+
+        System.out.println(strList.hashCode());
+
+
+
     }
 }

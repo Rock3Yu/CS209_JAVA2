3420808d3c34f53dc4a8ec5233f315373a8ded71
Rock3Yu <rockyu787@gmail.com>
2023-06-07 12:24:20 +0800

    before final 2

--- src/BeforeFinal.java
+++ src/BeforeFinal.java
@@ -8,6 +8,16 @@ public class BeforeFinal {
         Method m = clazz.getMethod("indexOf", String.class, int.class);
         String str = "This is a sentence in the program.";
         System.out.println(m.invoke(str, "i", 3));
+        // quiz 1
+        interface A {
+
+            int aMethod(String s);
+        }
+//        A a = a -> a.length();
+//        A x = y -> {return y;};
+        A b = (String s) -> 1;
+//        A s = "2" -> Integer.parseInt(s);
+        System.out.println(b.aMethod("hello"));
     }
 
 }
